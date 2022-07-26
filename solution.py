d9f22bb31f2314d7553d74cf56f3b90b074ea85f
upitroma <55750919+upitroma@users.noreply.github.com>
2022-07-26 14:25:51 -0500

    pretty comments

--- solution.py
+++ solution.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+
+# 1. install stockfish on your system
+# 2. pip3 install stockfish requests
+# 3. ./solution.py
+
 from stockfish import Stockfish
 import requests
 
@@ -9,22 +15,17 @@ game=[]
 
 stockfish.set_position(game)
 
-  
-
-    
-
 def sendWebRequest(move):
     url="http://localhost:5000/?userId=upitroma&move="+move
     response = requests.get(url).json().get("data")
     return response
-
-
 
 while True:
     AIMove=stockfish.get_best_move()
     print("My move: "+AIMove)
     game.append(AIMove)
 
+    # isn't required but cool to look at
     stockfish.set_position(game)
     print(stockfish.get_board_visual())
 
@@ -33,4 +34,6 @@ while True:
     game.append(OpponentMove)
 
     stockfish.set_position(game)
+
+    # isn't required but cool to look at
     print(stockfish.get_board_visual())
